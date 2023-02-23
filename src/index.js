13adc5c37a0e9d5da3c94573e87d475adc9f8ed2
David <david-williams-1996@outlook.com>
2023-02-23 19:04:23 +1100

    Add if statement to determine page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import pageLoad from "./pageLoad";
+import homePage from "./home";
 
 pageLoad();
+
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if(button.id == "home") {
+        homePage();
+    } else {
+        alert("works");
+    }
+  }) 
+})
+
+
