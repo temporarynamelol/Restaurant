849afa90ba38b73ebb4c079d31ba30beeb974f8d
David <david-williams-1996@outlook.com>
2023-02-23 19:06:06 +1100

    Create function to load home paget

---
+++ src/home.js
@@ -0,0 +1,20 @@
+import pic from './assets/cake.jpg';
+
+function homePage () {
+    const content = document.getElementById("content");
+
+    const title = document.createElement("h1");
+    const description = document.createElement("p");
+    const photo = document.createElement("img");
+
+    title.innerHTML = "Bakery";
+    description.innerHTML = "Best bakery ever";
+    photo.src = pic;
+
+    content.appendChild(title);
+    content.appendChild(description);
+    content.appendChild(photo);
+
+}
+
+export default homePage;
