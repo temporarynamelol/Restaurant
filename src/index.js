2bd2987ee955ad2dbfb4b06f89cbbe7b5d4e3d17
David <david-williams-1996@outlook.com>
2023-02-24 18:44:17 +1100

    Import menu function and add to button logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {pageLoad} from "./pageLoad";
 import {homePage} from "./home";
+import {menu} from "./menu";
 import {clean} from "./clean";
 
 pageLoad();
@@ -10,10 +11,11 @@ const buttons = document.querySelectorAll(".button");
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     if(button.id == "home") {
-        homePage();
-    } else {
         clean();
-        
+        homePage();
+    } else if (button.id == "menu") {
+        clean();
+        menu();
     }
   }) 
 })
